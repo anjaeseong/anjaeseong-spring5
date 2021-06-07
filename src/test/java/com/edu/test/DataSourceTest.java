9ea08bc5e524f4b42f6e546703fb693a6f331111
user <user@LAPTOP-49JTOBLQ>
2021-06-07 11:15:13 +0900

    3교시 커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -38,7 +38,7 @@ public class DataSourceTest {
 	public void oldQueryTest() throws Exception{
 		//스프링빈을 사용하지 않을때 예전방식: 코딩테스트에서는 스프링설정을 안쓰고 직접DB 아이디/암호 입력
 		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@Localhost:1521/XE","XE","apmsetup");
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@Localhost:1521/XE","XE2","apmsetup");
 		logger.debug("데이터베이스 직접 접속이 성공하였습니다 DB종류는"+connection.getMetaData().getDatabaseProductName());
 		//직접쿼리를 날립니다. 날리기전 쿼리문장 객체생성 statement
 		Statement stmt = connection.createStatement();
