0baf47426e8b940ace1e6485da1c1dbaf54d7551
user <user@LAPTOP-49JTOBLQ>
2021-06-21 17:48:21 +0900

    8교시 커밋

--- src/main/java/com/edu/service/BoardServiceImpl.java
+++ src/main/java/com/edu/service/BoardServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edu.dao.IF_BoardDAO;
 import com.edu.vo.AttachVO;
@@ -33,6 +34,7 @@ public class BoardServiceImpl implements IF_BoardService {
 		return boardDAO.countBoard(pageVO);
 	}
 
+	@Transactional //All or NotAll
 	@Override
 	public void deleteBoard(int bno) throws Exception {
 		// TODO 게시물 삭제 할때, 3개의 메서드가 실행(댓글+첨부파일삭제 -> 게시물이 삭제됨)
@@ -44,13 +46,31 @@ public class BoardServiceImpl implements IF_BoardService {
 		boardDAO.deleteBoard(bno);
 	}
 
+	@Transactional
 	@Override
 	public void updateBoard(BoardVO boardVO) throws Exception {
 		// TODO 첨부파일이 있으면 updateAttach -> 게시물 업데이트 updateBoard
-		boardDAO.updateAttach(null);
 		boardDAO.updateBoard(boardVO);
+		// 첨부파일 DB처리(아래)
+		int bno = boardVO.getBno();
+		String[] save_file_names = boardVO.getSave_file_names();
+		String[] real_file_names = boardVO.getReal_file_names();
+		if(save_file_names == null) { return; }//조건이 맞지않으면 빠져나감. 이후 실행않함.
+		AttachVO attachVO = new AttachVO();
+		int index = 0;
+		String real_file_name = "";
+		for(String save_file_name:save_file_names) {
+			real_file_name = real_file_names[index];
+			attachVO.setBno(bno);
+			attachVO.setSave_file_name(save_file_name);
+			attachVO.setReal_file_name(real_file_name);
+			boardDAO.updateAttach(attachVO);
+			index = index + 1;//index++;
+		}
+				
 	}
 
+	@Transactional 
 	@Override
 	public BoardVO readBoard(int bno) throws Exception {
 		// TODO 게시물 상세보기시 실행순서 readBoard -> updateViewCount 2개의 메서드가 필요
@@ -59,20 +79,27 @@ public class BoardServiceImpl implements IF_BoardService {
 		return boardVO;
 	}
 
+	@Transactional
 	@Override
 	public void insertBoard(BoardVO boardVO) throws Exception {
 		// TODO [부모]게시물 insertBoard -> [자식] 첨부파일 있으면 첨부파일 insertAttach
-		//게시물  등록
-		boardDAO.insertBoard(boardVO);
+		//게시물  등록 + 반환값으로 bno 추가
+		int bno = boardDAO.insertBoard(boardVO);
 		//첨부파일 등록: 1개 이상일때 가정해서 처리
-		String[] save_file_names=null;//폴더에 저장용 파일명들
-		String[] real_file_names=null;//UI용 배열 파일명들
+		String[] save_file_names=boardVO.getSave_file_names();//폴더에 저장용 파일명들
+		String[] real_file_names=boardVO.getReal_file_names();//UI용 배열 파일명들
+		if(save_file_names == null) { return; }//리턴이 발생되면, 이후 실행 않됨.
+		//첨부파일이 null 이 아닐때 아래가 진행됨.
 		int index = 0;
 		String real_file_name = "";//UI용 1개 파일명
-		if(save_file_names == null) { return; }//리턴이 발생되면, 이후 실행 않됨.
+		AttachVO attachVO = new AttachVO();
 		for(String save_file_name:save_file_names) {//첨부파일 개수만큼 반복진행
-			
-			boardDAO.insertAttach(null);
+			real_file_name = real_file_names[index];
+			attachVO.setBno(bno);
+			attachVO.setReal_file_name(real_file_name);
+			attachVO.setSave_file_name(save_file_name);
+			boardDAO.insertAttach(attachVO);
+			index++;//index = index + 1;
 		}
 	}
 
--- src/main/java/com/edu/vo/BoardVO.java
+++ src/main/java/com/edu/vo/BoardVO.java
@@ -3,14 +3,14 @@ package com.edu.vo;
 import java.util.Date;
 
 /**
- * 이 클래스는 게시물 CRUD에 필요한 멤버변수(필드)가 정의된 클래스 입니다
- * 목적은 데이터 변수값 이동시 겟셋 작업이 주 목적입니다
- * @author 안재성
+ * 이 클래스는 게시물 CRUD에 필요한 멤버변수(필드)가 정의된 클래스 입니다.
+ * 목적은 데이터 변수값 이동시 Get/Set 작업이 주 목적입니다.
+ * @author 김일국
  *
  */
 public class BoardVO {
 	//멤버변수 생성
-	private Integer bno; //PK
+	private Integer bno;//PK
 	private String title;
 	private String content;
 	private String writer;
@@ -19,6 +19,29 @@ public class BoardVO {
 	private Date reg_date;
 	private Date update_date;
 	private String board_type;//FK
+	//private String save_file_name; 첨부파일이 1개 이상일수 있기 때문에...
+	//게시물 1개 등록시, 첨부파일 1개이상 필요합니다. 그런데, 1개의 필드에 여러개를 입력할 순 없습니다.
+	//그래서, DB(테이블)에는 없는 가상 멤버변수(필드)를 2개 만듭니다.
+	private String[] save_file_names;//Null가능
+	private String[] real_file_names;//Null가능
+	
+	
+	public String[] getSave_file_names() {
+		return save_file_names;
+	}
+
+	public void setSave_file_names(String[] save_file_names) {
+		this.save_file_names = save_file_names;
+	}
+
+	public String[] getReal_file_names() {
+		return real_file_names;
+	}
+
+	public void setReal_file_names(String[] real_file_names) {
+		this.real_file_names = real_file_names;
+	}
+
 	//디버그용 toString()생성
 	@Override
 	public String toString() {
@@ -26,13 +49,11 @@ public class BoardVO {
 				+ ", view_count=" + view_count + ", reply_count=" + reply_count + ", reg_date=" + reg_date
 				+ ", update_date=" + update_date + ", board_type=" + board_type + "]";
 	}
-
-	// GET/SET생성
+	
+	//Get,Set 생성
 	public Integer getBno() {
 		return bno;
-	}
-	
-	
+	}	
 	public void setBno(Integer bno) {
 		this.bno = bno;
 	}
